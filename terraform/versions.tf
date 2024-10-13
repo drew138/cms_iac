5a9b8558d1f779c2145afdbe9d2ab2917f217977
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-13 16:00:26 -0500

    fix: removes prefix from terraform backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,7 +5,6 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "cms-iac-terraform-state"
-    prefix = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
     acl = "bucket-owner-full-control"
