753969edc54c099a7f30a4d3d5ff032b44cd5cf0
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-13 16:11:24 -0500

    fix: adds key property to terraform s3 backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,6 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "cms-iac-terraform-state"
+    key    = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
     acl = "bucket-owner-full-control"
