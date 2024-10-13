3acef7960fbf00471f5b8bf3969773389625a5b2
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-13 15:57:30 -0500

    fix: chaanges default for aws_launch_template_ami_id

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
 variable "aws_launch_template_ami_id" {
   type        = string
-  default     = local.ami
+  default     = "ami-005fc0f236362e99f"
   description = "AMI ID for the launch template"
 }
